a6c2fe3d942b27b9d7b99a40e1d0c45e9939f5fc
isaac <calliper.jennings@gmail.com>
2017-06-05 20:35:09 -0400

    trouble starts here

--- server.js
+++ server.js
@@ -2,8 +2,6 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
-
-var database;
 var Message = mongoose.model('Message', {
     msg: String
 });
@@ -15,6 +13,8 @@ app.use(function(req, res, next){
     next();
 })
 
+app.get('/api/message', GetMessages);
+
 app.post('/api/message', function(req, res){
     console.log(req.body);
     var message = new Message(req.body);
@@ -22,16 +22,15 @@ app.post('/api/message', function(req, res){
     res.status(200);
 })
 
-function GetMessages(){
+function GetMessages(req, res){
     Message.find({}).exec(function(err, result){
-        console.log(result);
+        res.send(result);
     })
 }
 
 mongoose.connect("mongodb://localhost:27017/test", function(err,db){
     if(!err){
         console.log("we are connected to mongo");
-        GetMessages();
     }
 })
 var server = app.listen(5000, function(){
